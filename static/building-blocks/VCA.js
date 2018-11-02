58e758974063f78592628e2605526bc35678fd23
Nathaniel Ott Homer <nathaniel.ott.homer@gmail.com>
2018-11-02 10:32:36 -0400

    begin building amplifier methods

--- static/building-blocks/VCA.js
+++ static/building-blocks/VCA.js
@@ -5,15 +5,21 @@ export default class VCA extends React.Component {
         super(props)
 
         this.amplifier = this.props.audioContext.createGain()
-        this.amplifier.gain.setValueAtTime(0, this.props.audioContext.currentTime)
+        this.amplifier.gain.value = 0
+
+        this.setGain = this.setGain.bind(this)
     }
 
     componentDidUpdate (prevProps, prevState) {
         if (this.props.gate) {
-            this.amplifier.gain.setValueAtTime(1, this.props.audioContext.currentTime)
+            this.setGain(1)
         } else {
-            this.amplifier.gain.setValueAtTime(0, this.props.audioContext.currentTime)
+            this.setGain(0)
         }
+    }
+
+    setGain (newValue, atTime = this.props.audioContext.currentTime) {
+        this.amplifier.gain.setValueAtTime(newValue, atTime)
     }
     
     render () {
