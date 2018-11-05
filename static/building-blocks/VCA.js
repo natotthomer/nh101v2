eada3b7238f725c987b301dc1be7bf00474a9ec5
Nathaniel Ott Homer <nathaniel.ott.homer@gmail.com>
2018-11-04 22:00:24 -0500

    fix attack time delta issue; still cancelling all value changes after attack stage

--- static/building-blocks/VCA.js
+++ static/building-blocks/VCA.js
@@ -6,38 +6,49 @@ export default class VCA extends React.Component {
     constructor (props) {
         super(props)
 
+        this.state = {
+            time: 0.0
+        }
+
         this.amplifier = this.props.audioContext.createGain()
         this.amplifier.gain.value = 0
 
         this.updateGain = this.updateGain.bind(this)
         this.triggerEnvelope = this.triggerEnvelope.bind(this)
         this.updateAttack = this.updateAttack.bind(this)
+        this.loop = this.loop.bind(this)
     }
 
     componentDidMount () {
         buildCanvas(this.amplifier)
+        this._frameId = window.requestAnimationFrame(this.loop);
     }
 
     componentDidUpdate (prevProps, prevState) {
-        
-        this.triggerEnvelope(prevProps, prevState)
+        this.triggerEnvelope(prevProps)
         if (this.props.amplifierAttackTime !== prevProps.amplifierAttackTime) {
             this.updateAttack(prevProps, prevState)
         }
     }
 
+    loop () {
+        this.setState({ time: this.props.audioContext.currentTime })
+
+        this._frameId = window.requestAnimationFrame(this.loop);
+    }
+
     updateAttack (prevProps, prevState) {
-        // let timeSinceTrigger = undefined
         if (this.props.triggerStartTime) {
             const timeSinceTrigger = this.props.audioContext.currentTime - this.props.triggerStartTime
-
             const newRemainingAttackTime = this.props.amplifierAttackTime - timeSinceTrigger
+
             this.amplifier.gain.cancelScheduledValues(this.props.audioContext.currentTime)
-            this.updateGain(1, newRemainingAttackTime + this.props.audioContext.currentTime, 'linear')
+            this.updateGain(this.amplifier.gain.value)
+            this.updateGain(1, this.props.audioContext.currentTime + newRemainingAttackTime, 'linear')
         }
     }
 
-    triggerEnvelope (prevProps, prevState) {
+    triggerEnvelope (prevProps) {
         const { audioContext, currentKeys, amplifierAttackTime } = this.props
         const currentKey = currentKeys[currentKeys.length - 1]
         const prevKey = prevProps.currentKeys[prevProps.currentKeys.length - 1]
