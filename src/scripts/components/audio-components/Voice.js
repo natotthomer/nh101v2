c437f492ee864f9d6d15fe029df4daf032f31b11
Nathaniel Ott Homer <nathaniel.ott.homer@gmail.com>
2018-12-09 11:14:41 -0500

    convert Voice component to function

--- src/scripts/components/audio-components/Voice.js
+++ src/scripts/components/audio-components/Voice.js
@@ -5,50 +5,50 @@ import VCA from './VCA'
 import VCF from './VCF'
 import Echo from './Echo'
 
-export default class Voice extends Component {
-  render () {
-    const { audioContext, currentKeys, synth, gateStartTime } = this.props
-    
-    const childrenProps = {
-      vcaProps: { 
-        audioContext: audioContext,
-        currentKeys: currentKeys,
-        moduleParameters: synth.vca,
-        retrigger: synth.retrigger,
-        gateStartTime: gateStartTime
-      },
-      vcfProps: {
-        audioContext: audioContext,
-        currentKeys: currentKeys,
-        moduleParameters: synth.vcf,
-        retrigger: synth.retrigger,
-        gateStartTime: gateStartTime
-      },
-      vcoProps: synth.vcos.map(vcoData => ({ 
-        audioContext: audioContext,
-        currentKeys: currentKeys,
-        moduleParameters: vcoData 
-      })),
-      echoProps: {
-        audioContext: audioContext,
-        currentKeys: currentKeys,
-        moduleParameters: synth.echo
-      }
+const Voice = props => {
+  const { audioContext, currentKeys, synth, gateStartTime } = props
+  
+  const childrenProps = {
+    vcaProps: { 
+      audioContext: audioContext,
+      currentKeys: currentKeys,
+      moduleParameters: synth.vca,
+      retrigger: synth.retrigger,
+      gateStartTime: gateStartTime
+    },
+    vcfProps: {
+      audioContext: audioContext,
+      currentKeys: currentKeys,
+      moduleParameters: synth.vcf,
+      retrigger: synth.retrigger,
+      gateStartTime: gateStartTime
+    },
+    vcoProps: synth.vcos.map(vcoData => ({ 
+      audioContext: audioContext,
+      currentKeys: currentKeys,
+      moduleParameters: vcoData 
+    })),
+    echoProps: {
+      audioContext: audioContext,
+      currentKeys: currentKeys,
+      moduleParameters: synth.echo
     }
-
-    const { vcaProps, vcoProps, vcfProps, echoProps } = childrenProps
-    
-    return (
-      <React.Fragment>
-        <Echo {...echoProps} parentNode={audioContext.destination}>
-          <VCA {...vcaProps}>
-            <VCF {...vcfProps}>
-              <VCO {...vcoProps[0]} />
-              <VCO {...vcoProps[1]} />
-            </VCF>
-          </VCA>
-        </Echo>
-      </React.Fragment>
-    )
   }
+
+  const { vcaProps, vcoProps, vcfProps, echoProps } = childrenProps
+  
+  return (
+    <React.Fragment>
+      <Echo {...echoProps} parentNode={audioContext.destination}>
+        <VCA {...vcaProps}>
+          <VCF {...vcfProps}>
+            <VCO {...vcoProps[0]} />
+            <VCO {...vcoProps[1]} />
+          </VCF>
+        </VCA>
+      </Echo>
+    </React.Fragment>
+  )
 }
+
+export default Voice
